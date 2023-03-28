81c7e6874f7ab123dae88b7e8813053368c43b6b
Son Dang <minhson0506@gmail.com>
2023-03-28 22:18:40 +0300

    init nodejs project

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+require('dotenv').config()
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
+
+app.use(cors({
+    origin: '*', // allow to server to accept request from different origin
+}))
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control_Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    // pass to next layer of middleware
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Api for getting data from PRH'
+    })
+})
+
+export default app
